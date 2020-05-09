025de48d35ba0182bd72093dc8156b2d25adfd29
Nishant Kumar <nishantrajput1212@gmail.com>
2020-05-09 23:22:32 +0530

    Added script to setup both in one go
    Script has support to kill already running containers. 
    In case user want a fresh setup and remove the old containers completely

--- elk.sh
+++ elk.sh
@@ -1,4 +1,7 @@
 docker network create elk
 docker stop `docker ps | grep -i elastic | awk '{print $1}'`
 docker stop `docker ps | grep -i kibana | awk '{print $1}'`
-
+docker rm container `docker ps -a | grep -i elastic | awk '{print $1}'`
+docker rm container `docker ps -a | grep -i kibana | awk '{print $1}'`
+docker run -d --name elasticsearch --net elk -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.6.2
+docker run -d --link elasticsearch:elasticsearch --name kibana --net elk -p 5601:5601 kibana:7.6.2
